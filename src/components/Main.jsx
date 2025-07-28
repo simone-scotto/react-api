7fd58fd49078aee1ed8b290cb88811e900c95952
simone-scotto <simone.scottotkd@gmail.com>
2025-07-28 14:49:49 +0200

    refactor: reorganize API calls and enhance actor data handling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,10 @@ import { useState } from "react"
 export default function Main () {
 
     const urlActress = "https://lanciweb.github.io/demo/api/actresses/"
-    
+    const urlActors = "https://lanciweb.github.io/demo/api/actors/"
+
+
+    const [actorsData, setActorsData] = useState([])
 
 
     function  handleClickActress () {
@@ -23,8 +26,6 @@ export default function Main () {
     
     }
 
-    const urlActors = "https://lanciweb.github.io/demo/api/actors/"
-    const [actorsData, setActorsData] = useState([])
 
 
 
@@ -40,18 +41,45 @@ export default function Main () {
     
     } 
 
+
+    function handleClickAllactors () {
+
+        const fetchActress = fetch(urlActress)
+                            .then(res => res.json());
+        const fetchActors = fetch(urlActors)
+                            .then(res => res.json());
+        Promise.all([fetchActors, fetchActress])
+        .then(([actors, actress]) => {
+            const allActors = [...actors, ...actress]
+            setActorsData(allActors)
+
+        })
+      /*   fetch(urlActors, urlActress)
+        
+        .then(res => res.json())
+       
+        .then(data => {
+
+            console.log(data);
+            setActorsData([...urlActors.data, ...urlActress.data])
+            
+        }) */
+    }
+
     
     return (
         <>
         <section className="btn_section d-flex justify-content-center gap-3 p-3">
             <button onClick={handleClickActress} className="btn btn-primary ">Load Actress Cards</button>
             <button onClick={handleClickActors} className="btn btn-primary ">Load Actror Cards</button>
+            <button onClick={handleClickAllactors} className="btn btn-primary ">Load All Actror Cards</button>
+        
         </section>
         <section className="actors">
             <div className="row row-cols-1 row-cols-md-3 g-2">
-                {actorsData.map(actress => {
+                {actorsData.map((actress, index) => {
                     return (
-                        <div key={actress.id} className="col">
+                        <div key={index} className="col">
                             <div className="card h-100 d-flex flex-row">
                                 <div className=" row">
                                     <div className="col-4">
